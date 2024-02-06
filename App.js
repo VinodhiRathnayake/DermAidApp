a6059535fb2b723ee1e5aa57505aacda49d0c1e4
Jehan Fernando <jehanfernando2425@gmail.com>
2024-02-06 11:43:01 +0530

    Auto import fix

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import SigninDetailsScreen from './app/screens/SigninDetailsScreen';
-import SigninScreen from './app/screens/SigninScreen';
-import SignupScreen from './app/screens/SignupScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import SignupDetailsScreen from './app/screens/SignupDetailsScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import SigninDetailsScreen from "./app/screens/SigninDetailsScreen";
+import SigninScreen from "./app/screens/SigninScreen";
+import SignupScreen from "./app/screens/SignupScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import SignupDetailsScreen from "./app/screens/SignupDetailsScreen";
 
 export default function App() {
   return (
-    < WelcomeScreen/>
+    // < WelcomeScreen/>
 
-    // < SigninScreen/>
+    // <SigninScreen />
 
-    // < SignupScreen/>
+    // <SignupScreen />
 
-    // <SigninDetailsScreen/>
+    // <SigninDetailsScreen />
 
-    // < SignupDetailsScreen/>
-    
+    <SignupDetailsScreen />
   );
 }
-
