af89b6087357b18e3c17b0bfca41b3a155af6ebf
Vinodhi Rathnayake <vinodhirathnayake30@gmail.com>
2024-02-14 19:41:11 +0530

    About Us Screen details

--- app/screens/AboutUs.js
+++ app/screens/AboutUs.js
@@ -1,32 +1,78 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
 import Screen from '../components/Screen';
+import AppText from '../components/AppText';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 function AboutUs(props) {
   return (
     <Screen>
+        
 <Text>About us</Text>
-<Text>Skin diseases are a major golbal health concern, 
+
+<AppText style={styles.text}>Skin diseases are a major global health concern, 
     affecting millions of people worldwide. Early diagnosis
-    and treatement of skin diseases are crucial in prventing 
+    and treatement of skin diseases are crucial in preventing 
     complications and improving patient outcomes. However, traditional
     diagnostic methods such as clinical examinations and biopsies,
     can be time-consuming, expensive, and invasive.
-    </Text>
+    </AppText>
 
-    <Text>
-    DermAid app has the potential to revolutionize the diagnosis and management of skin
+    <AppText style={styles.text}>
+    Derm< Text style={styles.redText}>Aid</Text> app has the potential to revolutionize the diagnosis and management of skin
     diseases. By providing a more accessible, affordable, and non-invasive alternaive to traditional
     methods, the app could help to improve access to dermatology care for people in
     underserved areas and those with limited financial resources. The app could also help
     to detect skin diseases at an earlier stage when they are more likely to be treatable.
-</Text>
+</AppText>
+<View style={styles.contactContainer}>
+<MaterialCommunityIcons name="email" color="black" size={23} style={styles.icon}/>
+<Text style={styles.email}>supportdermaid@gmail.com</Text>
+</View>
+<View style={styles.contactContainer}>
+<MaterialCommunityIcons name="phone" color="black" size={23} style={styles.icon}/>
+<Text style={styles.email}>+94 7700000000</Text>
+</View>
+<Image
+          style={[styles.image]}
+          source={require("../assets/DermAidlogo.jpg")}
+        />
+
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  
+  text: {
+    fontWeight: 'bold', 
+    textAlign: 'center', 
+    lineHeight: 27,
+    marginBottom:30,
+  },
+  contactContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom:15,
+  },
+  icon: {
+    marginRight: 12, 
+  },
+  email: {
+    fontSize: 15,
+  },
+  image: {
+    width: 170,
+    height: 70,
+    alignSelf: 'center',
+    
+  },
+  redText: {
+    color: 'red',
+    fontWeight: 'bold',
+  },
+ 
 });
 
 export default AboutUs;
