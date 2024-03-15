25b3574937f88447f3cc421756146fd3e8db6923
Jehan Fernando <jehanfernando2425@gmail.com>
2024-03-15 22:53:46 +0530

    making the sign up function

--- app/screens/SignupScreen.js
+++ app/screens/SignupScreen.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Formik } from "formik";
-import { View, TouchableOpacity } from "react-native";
+import { View, TouchableOpacity, ActivityIndicator } from "react-native";
 import { Octicons, Entypo } from "@expo/vector-icons";
-import DateTimePicker from "@react-native-community/datetimepicker";
 
 import {
   StyledContainer,
@@ -28,23 +27,38 @@ import {
 
 const SignUpScreen = ({ navigation }) => {
   const [isHidden, setHidden] = useState(true);
-  const [show, setShow] = useState(false);
-  const [date, setDate] = useState(new Date(2024, 0, 1));
+  const [message, setMessage] = useState();
+  const [messageType, setMessageType] = useState();
 
-  const [dob, setDob] = useState();
+  const handleSignUp = (credentials, setSubmitting) => {
+    handleMessage(null);
+    const url =
+      "https://polar-inlet-39847-1b8c485839ae.herokuapp.com/user/signup";
 
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date;
-    setShow(false);
-    setDate(currentDate);
-    setDob(currentDate);
+    axios
+      .post(url, credentials)
+      .then((response) => {
+        const result = response.data;
+        const { message, data, status } = result;
+
+        if (status !== "SUCCESS") {
+          handleMessage(message, status);
+        } else {
+          navigation.navigate("Welcome", { ...data });
+        }
+        setSubmitting(false);
+      })
+      .catch((err) => {
+        console.log(err.JSON());
+        setSubmitting(false);
+        handleMessage("An error occured. Check your internet connection");
+      });
   };
 
-  const showDateTimePicker = () => {
-    console.log("Opening the picker");
-    setShow(true);
+  const handleMessage = (message, type = "FAILED") => {
+    setMessage(message);
+    setMessageType(type);
   };
-
   return (
     <StyledContainer>
       <StatusBar style="dark" />
@@ -52,30 +66,39 @@ const SignUpScreen = ({ navigation }) => {
         <PageTitle>Derm Aid</PageTitle>
         <SubTitle>Account Signup</SubTitle>
 
-        {show && (
-          <DateTimePicker
-            testID="dateTimePicker"
-            value={date}
-            mode="date"
-            is24Hour={true}
-            onChange={onChange}
-          />
-        )}
-
         <Formik
           initialValues={{
-            fullName: "",
+            name: "",
             email: "",
             dateOfBirth: "",
             password: "",
             confirmPassword: "",
           }}
-          onSubmit={(values) => {
-            console.log(values);
-            navigation.navigate("Login");
+          onSubmit={(values, { setSubmitting }) => {
+            if (
+              values.email == "" ||
+              values.password == "" ||
+              values.name == "" ||
+              values.confirmPassword == "" ||
+              values.dateOfBirth == ""
+            ) {
+              handleMessage("Please fill all the fields");
+              setSubmitting(false);
+            } else if (values.password !== values.confirmPassword) {
+              handleMessage("The passwords do not match");
+              setSubmitting(false);
+            } else {
+              handleSignUp(values, setSubmitting);
+            }
           }}
         >
-          {({ handleChange, handleBlur, handleSubmit, values }) => (
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            isSubmitting,
+          }) => (
             <StyledFormArea>
               <MyTextInput
                 label="Full Name"
@@ -97,16 +120,13 @@ const SignUpScreen = ({ navigation }) => {
                 keyboardType="email-address"
               />
               <MyTextInput
-                label="Date of Birth"
-                icon="calendar"
-                placeholder="YYYY - MM - DD"
+                label="Full Name"
+                icon="person"
+                placeholder="Jehan Fernando"
                 placeholderTextColor={Colors.darklight}
                 onChangeText={handleChange("dateOfBirth")}
                 onBlur={handleBlur("dateOfBirth")}
-                value={dob ? dob.toDateString() : ""}
-                isDate={true}
-                // editable={false}
-                showDateTimePicker={showDateTimePicker}
+                value={values.dateOfBirth}
               />
               <MyTextInput
                 label="Password"
@@ -134,13 +154,20 @@ const SignUpScreen = ({ navigation }) => {
                 isHidden={isHidden}
                 setHidden={setHidden}
               />
-              <MsgBox>...</MsgBox>
-              <StyledButton onPress={handleSubmit}>
-                <ButtonText>SignUp</ButtonText>
-              </StyledButton>
+              <MsgBox type={messageType}>{message}</MsgBox>
+              {!isSubmitting && (
+                <StyledButton onPress={handleSubmit}>
+                  <ButtonText>SignUp</ButtonText>
+                </StyledButton>
+              )}
+              {isSubmitting && (
+                <StyledButton disabled={true}>
+                  <ActivityIndicator size="large" color={Colors.primary} />
+                </StyledButton>
+              )}
               <Line />
               <ExtraView>
-                <ExtraText>Alreaqdy have an account?</ExtraText>
+                <ExtraText>Already have an account?</ExtraText>
                 <TextLink
                   onPress={() => {
                     navigation.navigate("Login");
