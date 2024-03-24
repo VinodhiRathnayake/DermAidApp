d3a68b59966b4c3147ff5dfce54e15b9855522e9
Jehan Fernando <jehanfernando2425@gmail.com>
2024-03-25 01:12:45 +0530

    deleting the unused components

--- app/components/Forms/index.js
+++
@@ -1,5 +0,0 @@
-//Exporting statements
-export { default as AppForm } from "./AppForm";
-export { default as AppFormField } from "./AppFormField";
-export { default as ErrorMessage } from "./ErrorMessage";
-export { default as SubmitButton } from "./SubmitButton";
